74ba235d811180db8ac91f5ac89cf2ba9f8018b3
Junior Emilio De Leon Diaz <jun.emiliodiaz@gmail.com>
2021-12-17 22:02:30 -0400

    components ios and android

--- src/screens/components/Controls.tsx
+++ src/screens/components/Controls.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Text, TouchableNativeFeedbackBase, TouchableNativeFeedback, Platform } from 'react-native';
 
 interface IControl {
     ControlHandler: () => {} //or function,
@@ -9,16 +9,42 @@ interface IControl {
 }
 
 export const Controls = ({ControlHandler, ButtonColor, Title, ControlLocation}: IControl) => {
-    return (
-        <TouchableOpacity
-        onPress={() => ControlHandler()}
-        style={ControlLocation}
-        >
-            <View style={[styles.ControlWrapper, ButtonColor]}>
-                <Text style={styles.buttonText}>{Title}</Text>
-            </View>
-        </TouchableOpacity>
-    );
+
+    const iOS = () =>{
+        return (
+            <TouchableOpacity
+            activeOpacity={0.75}
+            onPress={() => ControlHandler()}
+            style={ControlLocation}
+            >
+                <View style={[styles.ControlWrapper, ButtonColor]}>
+                    <Text style={styles.buttonText}>{Title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    }
+
+    const androidOS = () => {
+        return (
+            <TouchableOpacity
+            onPress={() => ControlHandler()}
+            style={ControlLocation}
+            >
+                <TouchableNativeFeedback
+                onPress={() => ControlHandler()}
+                background={ TouchableNativeFeedback.Ripple("#28425B", false)}
+                >
+                    <View style={[styles.ControlWrapper, ButtonColor]}>
+                        <Text style={styles.buttonText}>{Title}</Text>
+                    </View>
+                </TouchableNativeFeedback>
+                
+            </TouchableOpacity>
+        );
+    }
+
+    return (Platform.OS === 'ios') ? iOS() : androidOS();
+    
 }
 
 const styles = StyleSheet.create({
